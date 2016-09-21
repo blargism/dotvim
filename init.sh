9fc8f1bdcf031b2fc0d30a3e9f5fdd973fe8e066
Joe Mills <mills.joe@gmail.com>
2016-09-21 15:16:07 -0500

    Removing command t

--- init.sh
+++ init.sh
@@ -4,9 +4,3 @@ echo "Initializing modules"
 git submodule init
 echo "Updating modules"
 git submodule update
-
-echo "Building command-t"
-cd ~/.vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
-
